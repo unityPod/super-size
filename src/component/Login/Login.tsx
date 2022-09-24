26af5e3d3df6fbfd483e2d2b6476d94783f956a2
unityPod <112590118+unityPod@users.noreply.github.com>
2022-09-24 10:21:24 -0700

    Update login styling page

--- src/component/Login/Login.tsx
+++ src/component/Login/Login.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { useState, useContext, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import UserContext from "../AuthContext/AuthContext";
-// import styles from "./Login.module.css";
+import styles from "./Login.module.css";
 import config from "../../config";
 import login from "../../assets/login.jpg";
+import LoginImg from "../../assets/login-img.png";
 
 function Login(){
     const auth = getAuth(config);
@@ -41,16 +42,20 @@ function Login(){
 
     return (
         <>
-        
-        <div>
-            <img/>
-            <div>
-                <p>Welcome back, please login into your account</p>
-                <h2>Email</h2>
-                <input type={"email"} placeholder="please enter your email" onChange={(e) => setEmail(e.target.value)}/>
-                <h2>Password</h2>
-                <input type={"password"} placeholder="please enter your password" onChange={(e) => setPassword(e.target.value)}/>
-                <button onClick={signIn}>Sign In</button>
+        <div className={styles["container"]}>
+            <div className={styles["inner-container"]}>
+                <h2 className={styles["title"]}>Super Size</h2>
+                <h1 className="font-primary text-[16px] leading-8">Sign In to this very serious fitness app</h1>
+                <h2 className="font-primary text-[16px] leading-8">If you don't have an account <Link to="/signup">Register Now</Link></h2>
+            </div>
+            <img className={styles["login-img"]} src={LoginImg}/>
+            <div className={styles["inner-container"]}>
+                <p className="font-primary font-bold text-[16px] leading-8">Welcome back, please login into your account</p>
+                <h2 className="font-primary text-[16px] leading-8">Email</h2>
+                <input className={styles["input"]} type={"email"} placeholder="please enter your email" onChange={(e) => setEmail(e.target.value)}/>
+                <h2 className="font-primary text-[16px] leading-5">Password</h2>
+                <input className={styles["input"]} type={"password"} placeholder="please enter your password" onChange={(e) => setPassword(e.target.value)}/>
+                <button className={styles["button"]} onClick={signIn}>Sign In</button>
             </div>
         </div>
         </>
